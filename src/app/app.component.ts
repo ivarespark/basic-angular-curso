d4214df89763e08167986e473a65466b9c6dd18e
Ivan Espinosa <ivarespar@gmail.com>
2023-10-04 02:37:12 -0500

    [o4] Creacion de componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // muestra un archivo
+  //template: '<h2>Curso de angular</h2>', // muesta codigo HTML directamente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
