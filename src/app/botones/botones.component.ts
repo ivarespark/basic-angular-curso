2d258cf0a32ccf77b5048f7be05ebe623edcab50
Ivan Espinosa <ivarespar@gmail.com>
2023-10-06 00:18:04 -0500

    [o4] Enlace de clases

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-botones',
   templateUrl: './botones.component.html',
   styleUrls: ['./botones.component.css']
 })
-export class BotonesComponent {
+export class BotonesComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {    
+  }
+
+  // Declaro propiedad
+  texto_color:String = '';
+  button_disabled:boolean = false;
 
 }
