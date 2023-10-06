8fb90844adce58db5b70dc34333cd3f9ade3f3e7
Ivan Espinosa <ivarespar@gmail.com>
2023-10-06 01:04:20 -0500

    [o4] Forms: variables de plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { BotonesComponent } from './botones/botones.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    BotonesComponent
+    BotonesComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
